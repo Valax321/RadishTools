144d651555e48fab35e8a321c0b1b901911e81c0
Audrey Castillo <valax321@gmail.com>
2023-06-09 16:31:36 +1000

    Gizmo/handle color stack

---
+++ Common/GizmoColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace OrbHall
+{
+    public readonly struct GizmoColor : IDisposable
+    {
+        private readonly Color m_OldColor;
+        
+        public GizmoColor(Color color)
+        {
+            m_OldColor = Gizmos.color;
+            Gizmos.color = color;
+        }
+
+        public void Dispose()
+        {
+            Gizmos.color = m_OldColor;
+        }
+    }
+}
---
+++ CommonEditor/HandlesColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace OrbHall
+{
+    public readonly struct HandleColor : IDisposable
+    {
+        private readonly Color m_OldColor;
+        
+        public HandleColor(Color color)
+        {
+            m_OldColor = Handles.color;
+            Handles.color = color;
+        }
+
+        public void Dispose()
+        {
+            Handles.color = m_OldColor;
+        }
+    }
+}
