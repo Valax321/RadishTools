9cba097a9a6a4bb5fd0f61be82250ffc54e2e4af
Audrey Castillo <valax321@gmail.com>
2023-06-09 16:32:40 +1000

    FoldoutHeaderGroup utility function

---
+++ CommonEditor/OrbEditorGUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace OrbHall
+{
+    public static class OrbEditorGUI
+    {
+        public static bool EditorPrefsFoldoutHeaderGroup(string key, GUIContent content, GUIStyle style = null,
+            Action<Rect> menuAction = null, GUIStyle menuIcon = null)
+        {
+            var keyState = EditorPrefs.GetBool(key, true);
+            var returnValue = EditorGUILayout.BeginFoldoutHeaderGroup(keyState, content, style, menuAction, menuIcon);
+            EditorPrefs.SetBool(key, returnValue);
+            return returnValue;
+        }
+    }
+}
