89ee30645cce4f12296c43e65d977ba52212b9e4
Audrey Castillo <valax321@gmail.com>
2024-02-27 08:52:41 +1100

    Allow direct comparison of scene references

--- Common/AssetManagement/SoftSceneReference.cs
+++ Common/AssetManagement/SoftSceneReference.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Radish
 {
     [Serializable]
-    public sealed class SoftSceneReference : ISoftObjectReference
+    public sealed class SoftSceneReference : ISoftObjectReference, IEquatable<SoftSceneReference>
     {
         [SerializeField] private string m_Guid;
 
@@ -19,6 +19,33 @@ namespace Radish
 #else
             return string.Format("{0} (Scene)", UnityEditor.AssetDatabase.GUIDToAssetPath(m_Guid));
 #endif
+        }
+
+        public static bool operator ==(SoftSceneReference lhs, SoftSceneReference rhs)
+        {
+            return lhs?.Equals(rhs) ?? false;
+        }
+        
+        public static bool operator !=(SoftSceneReference lhs, SoftSceneReference rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override int GetHashCode()
+        {
+            return (m_Guid != null ? m_Guid.GetHashCode() : 0);
+        }
+
+        public bool Equals(SoftSceneReference other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return m_Guid == other.m_Guid;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is SoftSceneReference other && Equals(other);
         }
     }
 }
