bc5100c5b865aeb6f0e7c7cb6b729cc47300744f
Audrey Castillo <valax321@gmail.com>
2024-07-26 15:50:27 +1000

    Deconstructor for IKeyValuePair

--- Runtime/IKeyValuePair.cs
+++ Runtime/IKeyValuePair.cs
@@ -5,4 +5,14 @@
         TKey key { get; set; }
         TValue value { get; set; }
     }
+
+    public static class KeyValuePairExtensions
+    {
+        public static void Deconstruct<TKey, TValue>(this IKeyValuePair<TKey, TValue> data, out TKey outKey,
+            out TValue outValue)
+        {
+            outKey = data.key;
+            outValue = data.value;
+        }
+    }
 }
